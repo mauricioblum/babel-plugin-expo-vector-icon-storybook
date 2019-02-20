ca7133f785ed83a8b9822aebf965d5d49873be83
Jesse Katsumata <jesse.katsumata@gmail.com>
2019-02-21 07:04:36 +0900

    :white_check_mark: add testcases

--- test/index.test.js
+++ test/index.test.js
@@ -20,8 +20,29 @@ it("transforms @expo/vector-icons to react-native-vector-icons", () => {
 it("transforms multiple imports", () => {
   const code_before =
     'import { MaterialIcon, Ionicon } from "@expo/vector-icons"';
-  const code_after =
-    'import MaterialIcon from "react-native-vector-icons/dist/MaterialIcon";import Ionicon from "react-native-vector-icons/dist/Ionicon"';
+  const code_after = `
+    import MaterialIcon from "react-native-vector-icons/dist/MaterialIcon";
+    import Ionicon from "react-native-vector-icons/dist/Ionicon"
+    `;
+
+  const actual = babel.transform(code_before, {
+    plugins: [pluginPath]
+  }).code;
+
+  const expected = babel.transform(code_after).code;
+
+  expect(actual).toBe(expected);
+});
+
+it("does not transform unrelated imports", () => {
+  const code_before = `
+    import * as React from 'react'
+    import { MaterialIcon } from '@expo/vector-icons'
+  `;
+  const code_after = `
+    import * as React from 'react'
+    import MaterialIcon from 'react-native-vector-icons/dist/MaterialIcon'
+  `;
 
   const actual = babel.transform(code_before, {
     plugins: [pluginPath]
